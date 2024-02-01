76d0a20455a4884dbca7ec01e9341cf94759a618
Vishwa2706 <vishwajayanth3@gmail.com>
2024-02-01 16:07:01 +0530

    pivot index file added.

---
+++ pivotIndex.js
@@ -0,0 +1,21 @@
+var pivotIndex = function(nums) {
+    const totalSum = nums.reduce((acc, num) => acc + num, 0);
+    let leftSum = 0;
+
+    for (let i = 0; i < nums.length; i++) {
+        const rightSum = totalSum - leftSum - nums[i];
+
+        if (leftSum === rightSum) {
+            return i;
+        }
+
+        leftSum += nums[i];
+    }
+
+    return -1;
+}
+
+// Example usage:
+const nums = [1, 7, 3, 6, 5, 6];
+const result = pivotIndex(nums);
+console.log(result); // Output: 3
